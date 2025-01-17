de61eb70ec84d51516d4882aaac041d437b7519e
pain-arch <himulol123@gmail.com>
2025-01-17 21:31:00 +0600

    add handling for custom AppError in error handler middleware

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -13,6 +14,13 @@ export const errorHandler: ErrorRequestHandler = (
   if (error instanceof SyntaxError) {
     return res.status(HTTPSTATUS.BAD_REQUEST).json({
       message: "Invalid JSON payload passed. Please check your request body and try again",
+    });
+  }
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      errorCode: error.errorCode,
     });
   }
 

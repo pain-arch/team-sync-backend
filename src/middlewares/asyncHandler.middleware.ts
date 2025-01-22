cc7644faafa32f69c3843e9ccd57c42c058dc538
pain-arch <himulol123@gmail.com>
2025-01-22 17:32:24 +0600

    refactor middlewares: improve asyncHandler and errorHandler, add isAuthenticated middleware

--- src/middlewares/asyncHandler.middleware.ts
+++ src/middlewares/asyncHandler.middleware.ts
@@ -1,13 +1,13 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
-type AsyncControllerType = (
+type AyncControllerType = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => Promise<any>;
 
 export const asyncHandler =
-  (controller: AsyncControllerType): AsyncControllerType =>
+  (controller: AyncControllerType): AyncControllerType =>
   async (req, res, next) => {
     try {
       await controller(req, res, next);
--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,6 +1,20 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
 import { HTTPSTATUS } from "../config/http.config";
 import { AppError } from "../utils/appError";
+import { z, ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum";
+
+const formatZodError = (res: Response, error: z.ZodError) => {
+  const errors = error?.issues?.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+  return res.status(HTTPSTATUS.BAD_REQUEST).json({
+    message: "Validation failed",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -8,13 +22,16 @@ export const errorHandler: ErrorRequestHandler = (
   res,
   next
 ): any => {
-
-    console.error(`Error Occured on PATH: ${req.path}`, error);
+  console.error(`Error Occured on PATH: ${req.path} `, error);
 
   if (error instanceof SyntaxError) {
     return res.status(HTTPSTATUS.BAD_REQUEST).json({
-      message: "Invalid JSON payload passed. Please check your request body and try again",
+      message: "Invalid JSON format. Please check your request body.",
     });
+  }
+
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
   }
 
   if (error instanceof AppError) {
@@ -25,7 +42,7 @@ export const errorHandler: ErrorRequestHandler = (
   }
 
   return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
-    message: "Internal server error",
+    message: "Internal Server Error",
     error: error?.message || "Unknow error occurred",
   });
 };

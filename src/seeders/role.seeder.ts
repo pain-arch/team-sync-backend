1c855537e1320893e8dd745854ba6c73b5f81eb4
pain-arch <himulol123@gmail.com>
2025-01-22 17:40:49 +0600

    refactor role seeder: enhance logging messages for clarity and consistency

--- src/seeders/role.seeder.ts
+++ src/seeders/role.seeder.ts
@@ -5,7 +5,7 @@ import RoleModel from "../models/roles-permission.model";
 import { RolePermissions } from "../utils/role-permission";
 
 const seedRoles = async () => {
-  console.log("Seeding roles...");
+  console.log("Seeding roles started...");
 
   try {
     await connectDatabase();
@@ -20,11 +20,10 @@ const seedRoles = async () => {
       const role = roleName as keyof typeof RolePermissions;
       const permissions = RolePermissions[role];
 
-      // Check if role already exists
+      // Check if the role already exists
       const existingRole = await RoleModel.findOne({ name: role }).session(
         session
       );
-
       if (!existingRole) {
         const newRole = new RoleModel({
           name: role,
@@ -33,24 +32,22 @@ const seedRoles = async () => {
         await newRole.save({ session });
         console.log(`Role ${role} added with permissions.`);
       } else {
-        console.log(`Role ${role} already exists`);
+        console.log(`Role ${role} already exists.`);
       }
     }
 
     await session.commitTransaction();
-    console.log("Transcation committed");
+    console.log("Transaction committed.");
 
     session.endSession();
-    console.log("Session ended");
+    console.log("Session ended.");
 
     console.log("Seeding completed successfully.");
   } catch (error) {
-    console.error("Seeding failed with error: ", error);
+    console.error("Error during seeding:", error);
   }
 };
 
-seedRoles().catch((error) => {
-    console.error("Error running seed script: ", error);
-})
-
-export default seedRoles;
+seedRoles().catch((error) =>
+  console.error("Error running seed script:", error)
+);

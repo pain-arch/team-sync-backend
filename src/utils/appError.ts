362d286adf726416064faabdadebf72c8cd8ec5d
pain-arch <himulol123@gmail.com>
2025-01-22 17:46:59 +0600

    refactor error handling and role permissions: streamline constructors, improve error messages, and enhance roleGuard utility

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -1,5 +1,4 @@
-import { HTTPSTATUS } from "./../config/http.config";
-import { HttpStatusCodeType } from "../config/http.config";
+import { HTTPSTATUS, HttpStatusCodeType } from "../config/http.config";
 import { ErrorCodeEnum, ErrorCodeEnumType } from "../enums/error-code.enum";
 
 export class AppError extends Error {
@@ -9,7 +8,7 @@ export class AppError extends Error {
   constructor(
     message: string,
     statusCode = HTTPSTATUS.INTERNAL_SERVER_ERROR,
-    errorCode: ErrorCodeEnumType
+    errorCode?: ErrorCodeEnumType
   ) {
     super(message);
     this.statusCode = statusCode;
@@ -19,54 +18,54 @@ export class AppError extends Error {
 }
 
 export class HttpException extends AppError {
-    constructor(
-      message = "Http Exception Error",
-      statusCode: HttpStatusCodeType,
-      errorCode: ErrorCodeEnumType
-    ) {
-      super(message, statusCode, errorCode);
-    }
+  constructor(
+    message = "Http Exception Error",
+    statusCode: HttpStatusCodeType,
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(message, statusCode, errorCode);
   }
+}
 
 export class InternalServerException extends AppError {
-    constructor(
-      message = "Internal Server Error",
-      errorCode?: ErrorCodeEnumType
-    ) {
-      super(
-        message,
-        HTTPSTATUS.INTERNAL_SERVER_ERROR,
-        errorCode || ErrorCodeEnum.INTERNAL_SERVER_ERROR
-      );
-    }
+  constructor(
+    message = "Internal Server Error",
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(
+      message,
+      HTTPSTATUS.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCodeEnum.INTERNAL_SERVER_ERROR
+    );
   }
-  
-  export class NotFoundException extends AppError {
-    constructor(message = "Resource not found", errorCode?: ErrorCodeEnumType) {
-      super(
-        message,
-        HTTPSTATUS.NOT_FOUND,
-        errorCode || ErrorCodeEnum.RESOURCE_NOT_FOUND
-      );
-    }
-  }
+}
 
-  export class BadRequestException extends AppError {
-    constructor(message = "Bad Request", errorCode?: ErrorCodeEnumType) {
-      super(
-        message,
-        HTTPSTATUS.BAD_REQUEST,
-        errorCode || ErrorCodeEnum.VALIDATION_ERROR
-      );
-    }
+export class NotFoundException extends AppError {
+  constructor(message = "Resource not found", errorCode?: ErrorCodeEnumType) {
+    super(
+      message,
+      HTTPSTATUS.NOT_FOUND,
+      errorCode || ErrorCodeEnum.RESOURCE_NOT_FOUND
+    );
   }
+}
 
-  export class UnauthorizedException extends AppError {
-    constructor(message = "Unauthorized Access", errorCode?: ErrorCodeEnumType) {
-      super(
-        message,
-        HTTPSTATUS.UNAUTHORIZED,
-        errorCode || ErrorCodeEnum.ACCESS_UNAUTHORIZED
-      );
-    }
+export class BadRequestException extends AppError {
+  constructor(message = "Bad Request", errorCode?: ErrorCodeEnumType) {
+    super(
+      message,
+      HTTPSTATUS.BAD_REQUEST,
+      errorCode || ErrorCodeEnum.VALIDATION_ERROR
+    );
   }
+}
+
+export class UnauthorizedException extends AppError {
+  constructor(message = "Unauthorized Access", errorCode?: ErrorCodeEnumType) {
+    super(
+      message,
+      HTTPSTATUS.UNAUTHORIZED,
+      errorCode || ErrorCodeEnum.ACCESS_UNAUTHORIZED
+    );
+  }
+}
--- src/utils/get-env.ts
+++ src/utils/get-env.ts
@@ -1,11 +1,10 @@
-export const getEnv = (key:string, defaultValue:string = "") => {
+export const getEnv = (key: string, defaultValue: string = ""): string => {
     const value = process.env[key];
-
     if (value === undefined) {
-        if (defaultValue) {
-            return defaultValue;
-        }
-        throw new Error(`Environment variable ${key} is not defined`);
+      if (defaultValue) {
+        return defaultValue;
+      }
+      throw new Error(`Enviroment variable ${key} is not set`);
     }
     return value;
-};
+  };
